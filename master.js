8567d7a3cc6aa937c5da9d912c59a8579e55accd
wrxavex <wrxavex@users.noreply.github.com>
2016-05-26 22:47:32 +0800

    Create master.js

---
+++ master.js
@@ -0,0 +1,19 @@
+var osc = require('osc-min'),
+    dgram = require('dgram'),
+    omx = require('omxcontrol'),
+    remote;
+
+function send() {
+  var x = osc.toBuffer({
+    oscType: 'message',
+    address: '/omxplayer',
+    args: [{
+      type: 'integer',
+      value: 1
+    }]
+  });
+  udp.send(x, 0, x.length, 9998, "192.168.1.189");
+  udp.send(x, 0, x.length, 9998, "192.168.1.159");
+  udp.send(x, 0, x.length, 9998, "192.168.1.160");
+}
+send();
