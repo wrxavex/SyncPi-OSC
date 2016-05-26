09b9381c7245f765c3e4161d97ee5938197fea51
wrxavex <wrxavex@users.noreply.github.com>
2016-05-26 23:19:16 +0800

    Update master.js

--- master.js
+++ master.js
@@ -2,20 +2,7 @@ var osc = require('osc-min'),
     dgram = require('dgram'),
     remote;
     
-sock = udp.createSocket("udp4", function(msg, rinfo) {
-  var error, error1, redirected;
-  try {
-    redirected = osc.applyAddressTransform(msg, function(address) {
-      return "/redirect" + address;
-    });
-    return sock.send(redirected, 0, redirected.length, outport, "localhost");
-  } catch (error1) {
-    error = error1;
-    return console.log("error redirecting: " + error);
-  }
-});
-
-sock.bind(inport);
+var udp = dgram.createSocket("udp4");
 
 function send() {
   var x = osc.toBuffer({
