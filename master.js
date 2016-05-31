5b4ccaef1ac82e00dff01e4efbe0bf2d39ca73d6
wrxavex <wrxavex@users.noreply.github.com>
2016-05-31 20:28:56 +0800

    Update master.js

--- master.js
+++ master.js
@@ -6,7 +6,13 @@ var udp = dgram.createSocket("udp4");
 
 function send() {
   var x = osc.toBuffer({
-    oscType: 'message',
+    oscType: 'message',  catch (err) {
+    console.log('could not decode OSC message');
+  }
+});
+omxcallback.bind(9999);
+// setInterval(send, 4000);
+
     address: '/omxplayer',
     args: [{
       type: 'integer',
@@ -14,7 +20,23 @@ function send() {
     }]
   });
   udp.send(x, 0, x.length, 9998, "192.168.1.189");
+  udp.send(x, 0, x.length, 9998, "192.168.1.142");
   udp.send(x, 0, x.length, 9998, "192.168.1.159");
   udp.send(x, 0, x.length, 9998, "192.168.1.160");
 }
+var omxcallback = dgram.createSocket('udp4', function(msg, rinfo){
+  try
+  {
+    console.log(osc.fromBuffer(msg));
+    console.log("get omx callback");
+    send();
+  }
+  catch (err) {
+    console.log('could not decode OSC message');
+  }
+});
+omxcallback.bind(9999);
+// setInterval(send, 4000);
+
+
 send();
