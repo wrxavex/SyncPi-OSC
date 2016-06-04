ee1d70ffd6797efc00080830a405decf26dd0b1e
wrxavex <wrxavex@users.noreply.github.com>
2016-06-04 17:50:59 +0800

    Update master.js

--- master.js
+++ master.js
@@ -6,37 +6,43 @@ var udp = dgram.createSocket("udp4");
 
 function send() {
   var x = osc.toBuffer({
-    oscType: 'message',  catch (err) {
-    console.log('could not decode OSC message');
-  }
-});
-omxcallback.bind(9999);
-// setInterval(send, 4000);
-
+    oscType: 'message',
     address: '/omxplayer',
     args: [{
       type: 'integer',
       value: 1
     }]
   });
-  udp.send(x, 0, x.length, 9998, "192.168.1.189");
-  udp.send(x, 0, x.length, 9998, "192.168.1.142");
-  udp.send(x, 0, x.length, 9998, "192.168.1.159");
-  udp.send(x, 0, x.length, 9998, "192.168.1.160");
+  udp.send(x, 0, x.length, 9998, "192.168.1.201");
+  udp.send(x, 0, x.length, 9998, "192.168.1.202");
+  udp.send(x, 0, x.length, 9998, "192.168.1.203");
+  udp.send(x, 0, x.length, 9998, "192.168.1.204");
+  udp.send(x, 0, x.length, 9998, "192.168.1.205");
+  udp.send(x, 0, x.length, 9998, "192.168.1.206");
+  udp.send(x, 0, x.length, 9998, "192.168.1.207");
+  udp.send(x, 0, x.length, 9998, "192.168.1.208");
+  udp.send(x, 0, x.length, 9998, "192.168.1.209");
+  udp.send(x, 0, x.length, 9998, "192.168.1.210");
+  udp.send(x, 0, x.length, 9998, "192.168.1.211");
+  udp.send(x, 0, x.length, 9998, "192.168.1.212");
 }
+
 var omxcallback = dgram.createSocket('udp4', function(msg, rinfo){
-  try
+  try 
   {
     console.log(osc.fromBuffer(msg));
     console.log("get omx callback");
     send();
-  }
+  } 
   catch (err) {
     console.log('could not decode OSC message');
   }
 });
-omxcallback.bind(9999);
-// setInterval(send, 4000);
 
+
+
+omxcallback.bind(9999);
+
+// setInterval(send, 4000);
 
 send();
