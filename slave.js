c55734f1e9e29f0585e547b191e3669b00146a99
wrxavex <wrxavex@gmail.com>
2016-06-22 21:08:20 +0800

    change ip

--- slave.js
+++ slave.js
@@ -7,6 +7,7 @@ var osc = require('osc-min'),
 var video_id = fs.readFileSync('/boot/set_id', 'utf8');
 video_id = video_id.replace(/(\r\n|\n|\r)/gm,"");
 
+var play_count = 0;
     
 // listen for OSC messages and print them to the console
 var udp = dgram.createSocket('udp4', function(msg, rinfo) {
@@ -16,6 +17,10 @@ var udp = dgram.createSocket('udp4', function(msg, rinfo) {
 
   try {
     console.log(osc.fromBuffer(msg));
+
+    play_count = play_count + 1;
+    var time = new Date();
+    console.log('Playing '+play_count+' times at '+time.toLocaleTimeString());
 
     omx.start('/home/pi/nmh/v-'+video_id+'.mp4')
   } catch (err) {
