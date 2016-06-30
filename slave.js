d202a8187c3150d0ae22c638b9a2494b160891aa
wrxavex <wrxavex@gmail.com>
2016-07-01 00:10:37 +0800

    catch message.args

--- slave.js
+++ slave.js
@@ -19,7 +19,8 @@ var udp = dgram.createSocket('udp4', function(msg, rinfo) {
     osc_message = osc.fromBuffer(msg);
     console.log('args[0]= '+osc_message.args[0].value);
     console.log('args[1]= '+osc_message.args[1].value);
-
+    if (osc_message.args[0] == 0)
+        console.log('it's master's message')
     play_count = play_count + 1;
     var time = new Date();
 
