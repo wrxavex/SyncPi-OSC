2110d3e82d9bcc175c8341d6ec389d1fd30f00a3
wrxavex <wrxavex@gmail.com>
2016-09-07 19:14:34 +0800

    add commit and send device status

--- get_ip.py
+++ get_ip.py
@@ -2,14 +2,33 @@ import socket
 import fcntl
 import struct
 
-def get_ip_address(ifname):
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    return socket.inet_ntoa(fcntl.ioctl(
-        s.fileno(),
-        0x8915,  # SIOCGIFADDR
-        struct.pack('256s', ifname[:15])
-    )[20:24])
-try:
-    myip = get_ip_address('eth0')
-except:
-    myip = 'no lan'
+# def get_ip_address(ifname):
+#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#     return socket.inet_ntoa(fcntl.ioctl(
+#         s.fileno(),
+#         0x8915,  # SIOCGIFADDR
+#         struct.pack('256s', ifname[:15])
+#     )[20:24])
+# try:
+#     myip = get_ip_address('eth0')
+# except:
+#     myip = 'no lan'
+
+
+# -*- coding: utf-8 -*-
+
+import socket, struct, fcntl
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sockfd = sock.fileno()
+SIOCGIFADDR = 0x8915
+
+def get_ip(iface = 'eth0'):
+    ifreq = struct.pack('16sH14s', iface.encode('utf-8'), socket.AF_INET, b'\x00'*14)
+    try:
+        res = fcntl.ioctl(sockfd, SIOCGIFADDR, ifreq)
+    except:
+        return None
+    ip = struct.unpack('16sH2x4s8x', res)[2]
+    return socket.inet_ntoa(ip)
+
+print(get_ip('eth0'))
