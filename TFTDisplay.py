475945c60a22bed05c65d63cd78136f09da52255
wrxavex <wrxavex@gmail.com>
2016-09-07 23:19:42 +0800

    add commit and send device status

--- TFTDisplay.py
+++ TFTDisplay.py
@@ -211,12 +211,15 @@ def display_main_info(time_now):
     text_surface_video_now = font_small.render(u'現在影片：%s' % ds.video_name, True, WHITE)
     text_surface_time = font_small.render(u'%s' % time_now, True, WHITE)
 
+    text_surface_device1 = font_small.render(u'count:%d' % ds.device_status[3])
+
     rect_hostname = text_surface_hostname.get_rect(center=(160, 18))
     rect_myip = text_surface_myip.get_rect(center=(160, 48))
     rect_player_mode = text_surface_player_mode.get_rect(center=(160,160))
     rect_cpu_temp = text_surface_cpu_temp.get_rect(center=(240, 216))
     rect_video_now = text_surface_video_now.get_rect(center=(160, 100))
     rect_time = text_surface_time.get_rect(center=(80, 216))
+    rect_device1 = text_surface_device1.get_rect(center=(80,200))
 
     lcd.blit(text_surface_hostname, rect_hostname)
     lcd.blit(text_surface_myip, rect_myip)
@@ -224,6 +227,7 @@ def display_main_info(time_now):
     lcd.blit(text_surface_cpu_temp, rect_cpu_temp)
     lcd.blit(text_surface_video_now, rect_video_now)
     lcd.blit(text_surface_time, rect_time)
+    lcd.blit(text_surface_device1, rect_device1)
 
 
 def display_set_player_mode(time_now):
