4f6e789c1b171038169c0850b2e31748842401e9
wrxavex <wrxavex@gmail.com>
2016-09-07 21:02:14 +0800

    add commit and send device status

--- TFTDisplay.py
+++ TFTDisplay.py
@@ -518,6 +518,13 @@ def main():
 if __name__ == '__main__':
     print ("Display info")
 
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip",
+                        default="127.0.0.1", help="The ip to listen on")
+    parser.add_argument("--port",
+                        type=int, default=9997, help="The port to listen on")
+    args = parser.parse_args()
+
     server = osc_server.ThreadingOSCUDPServer(
         (args.ip, args.port), dispatcher)
     print("Serving on {}".format(server.server_address))
