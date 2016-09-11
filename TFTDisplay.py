b1ab91fc69971362ef0739952ae4d0f046d9b81a
wrxavex <wrxavex@gmail.com>
2016-09-12 00:25:56 +0800

    change playing time display content

--- TFTDisplay.py
+++ TFTDisplay.py
@@ -239,8 +239,8 @@ def display_main_info(time_now):
     text_surface_device11 = font_small.render(u'11:%d' % ds.device_status[14], True, WHITE)
     text_surface_device12 = font_small.render(u'12:%d' % ds.device_status[15], True, WHITE)
 
-    text_surface_play_count = font_small.render(u'已播 %d次' % ds.device_status[3], True, WHITE)
-    text_surface_last_play = font_small.render(u'- %s' % playing_time, True, WHITE)
+    text_surface_play_count = font_small.render(u'影片已播:%d次==%s' % (ds.device_status[3], playing_time), True, WHITE)
+    # text_surface_last_play = font_small.render(u'- %s' % playing_time, True, WHITE)
 
     rect_hostname = text_surface_hostname.get_rect(center=(160, 18))
     rect_myip = text_surface_myip.get_rect(center=(160, 48))
@@ -249,7 +249,7 @@ def display_main_info(time_now):
     rect_video_now = text_surface_video_now.get_rect(center=(160,108))
 
     rect_play_count = text_surface_play_count.get_rect(center=(80,138))
-    rect_last_play = text_surface_last_play.get_rect(center=(240, 138))
+    # rect_last_play = text_surface_last_play.get_rect(center=(240, 138))
 
     rect_device1 = text_surface_device1.get_rect(center=(45, 165))
     rect_device2 = text_surface_device2.get_rect(center=(93, 165))
@@ -273,8 +273,10 @@ def display_main_info(time_now):
     lcd.blit(text_surface_cpu_temp, rect_cpu_temp)
     lcd.blit(text_surface_video_now, rect_video_now)
     lcd.blit(text_surface_time, rect_time)
-    lcd.blit(text_surface_last_play, rect_last_play)
+
+    # lcd.blit(text_surface_last_play, rect_last_play)
     lcd.blit(text_surface_play_count, rect_play_count)
+
     lcd.blit(text_surface_device1, rect_device1)
     lcd.blit(text_surface_device2, rect_device2)
     lcd.blit(text_surface_device3, rect_device3)
