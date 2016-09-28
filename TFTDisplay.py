a9321986c5b3e8bed7c6724e5ebda12436290b03
wrxavex <wrxavex@gmail.com>
2016-09-28 20:49:51 +0800

    add upload

--- TFTDisplay.py
+++ TFTDisplay.py
@@ -414,7 +414,7 @@ def check_option(time_now):
             if ds.btnevent == 1:
                 mode_modify = u'變更為獨立播放模式'
                 if ds.btnsubmit == 1:
-                    ds.btnsubmit = 0
+                    ds.btnsubmit = 1
                     ds.player_mode = 0
                     ds.btnevent = 0
                     ds.status = 1
@@ -423,7 +423,7 @@ def check_option(time_now):
             elif ds.btnevent == 2:
                 mode_modify = u'變更為同步播放主機'
                 if ds.btnsubmit == 1:
-                    ds.btnsubmit = 0
+                    ds.btnsubmit = 1
                     ds.player_mode = 1
                     ds.id_to_set = 1
                     ds.btnevent = 0
